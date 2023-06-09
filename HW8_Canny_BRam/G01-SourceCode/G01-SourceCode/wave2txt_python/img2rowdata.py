32cd45858b5b127a99ee1f371ef891abf83abec0
C110112171 東穎 <91197477+ing0331@users.noreply.github.com>
2023-06-09 16:55:21 +0800

    Delete HW8_Canny_BRam/G01-SourceCode directory

--- HW8_Canny_BRam/G01-SourceCode/G01-SourceCode/wave2txt_python/img2rowdata.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-img_path = "D:\\GSlab_git_NAS\\HW8_Canny_BRam\\N06-Tutorial\\road_org.jpg"
-img = cv2.imread(img_path)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-with open('input_data.txt', 'w') as file:
-    for row in gray:
-        file.write(',\n'.join(map(str, row)) + '\n')
